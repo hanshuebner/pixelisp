f9a416e70d23ed4e07e6efff5c7d16274a1040db
Hans Huebner <hans.huebner@lambdawerk.com>
2016-12-21 22:52:02 +0100

    Have Hunchentoot log to cl-log

--- webserver.lisp
+++ webserver.lisp
@@ -6,11 +6,28 @@
 
 (in-package :webserver)
 
+(defclass acceptor (hunchentoot:easy-acceptor)
+  ())
+
+(defmethod hunchentoot:acceptor-log-access ((acceptor acceptor) &key return-code)
+  (cl-log:log-message :info
+                      "(~A~@[ ~A~]) ~A ~A~@[?~A~] => ~A~@[ (~A bytes)~]"
+                      (hunchentoot:remote-addr*)
+                      (hunchentoot:authorization)
+                      (hunchentoot:request-method*)
+                      (hunchentoot:script-name*)
+                      (hunchentoot:query-string*)
+                      return-code
+                      (hunchentoot:content-length*)))
+
+(defmethod hunchentoot:acceptor-log-message ((acceptor acceptor) log-level format-string &rest format-arguments)
+  (cl-log:log-manager-message log-level "~?" format-string format-arguments))
+
 (defun start (&key port)
   (cl-log:log-message :info "Starting hunchentoot on port ~A" port)
   (messaging:make-agent :webserver
                         (lambda ()
-                          (let ((acceptor (make-instance 'hunchentoot:easy-acceptor
+                          (let ((acceptor (make-instance 'acceptor
                                                          :port port
                                                          :document-root #P "html/")))
                             (hunchentoot:start acceptor)
