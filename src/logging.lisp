02332e53065847c9e05f41f900ecc66ca6487452
Hans Huebner <hans.huebner@lambdawerk.com>
2016-12-28 10:12:20 +0100

    Make log file rotatable

--- src/logging.lisp
+++ src/logging.lisp
@@ -8,6 +8,8 @@
 
 (in-package :logging)
 
+(defparameter *logfile-name* #P"log/pixelisp.log")
+
 (defclass formatted-message (cl-log:formatted-message)
   ())
 
@@ -24,6 +26,38 @@
           (cl-log:message-description message)
           (cl-log:message-arguments message)))
 
+(defclass log-file-messenger (cl-log:text-stream-messenger)
+  ((filename :reader filename
+             :initarg :filename)
+   (external-format :reader external-format
+                    :initarg :external-format
+                    :initform :default)
+   (log-file-size :accessor log-file-size
+                  :initform most-positive-fixnum)))
+
+(defun file-size (filename)
+  (nth-value 2 (ccl::%stat (namestring filename))))
+
+(defun reopen (messenger)
+  (let ((stream (open (filename messenger)
+                      :direction :output
+                      :element-type :default
+                      :if-does-not-exist :create
+                      :if-exists :append
+                      :sharing :lock
+                      :external-format (external-format messenger))))
+    (setf (slot-value messenger 'cl-log::stream) stream
+          (log-file-size messenger) (file-size (filename messenger)))))
+
+(defmethod initialize-instance :after ((messenger log-file-messenger) &key)
+  (reopen messenger))
+
+(defmethod cl-log:messenger-send-message :before ((messenger log-file-messenger) (message t))
+  (let ((current-log-file-size (file-size (filename messenger))))
+    (when (or (null current-log-file-size)
+              (< current-log-file-size (log-file-size messenger)))
+      (reopen messenger))))
+
 (defun start ()
   (setf (cl-log:log-manager)
         (make-instance 'cl-log:log-manager :message-class 'formatted-message))
@@ -33,15 +67,9 @@
   (cl-log:start-messenger 'cl-log:ring-messenger
                           :name :log-ring
                           :length 100)
-  (let ((stream (open #P"log/pixelisp.log"
-                      :direction :output
-                      :element-type :default
-                      :if-does-not-exist :create
-                      :if-exists :append
-                      :sharing :lock)))
-    (cl-log:start-messenger 'cl-log:text-stream-messenger
-                            :name :logfile
-                            :stream stream)))
+  (cl-log:start-messenger 'log-file-messenger
+                          :name :logfile
+                          :filename *logfile-name*))
 
 (defun last-messages ()
   (cl-log:ring-messenger-messages (cl-log:find-messenger :log-ring)))
